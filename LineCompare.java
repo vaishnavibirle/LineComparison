dd5e5476c854bf73bd4fdec2ffb082064dcea594
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-28 23:50:10 +0530

    [vaishnavi] adding UC1

--- LineCompare.java
+++ LineCompare.java
@@ -1,10 +1,22 @@
 package com.company;
-
-public class LineCompare {
-//Displaying welcome message
-    public static void main(String[] args) {
-        {
-            System.out.println("Welcome to Line Comparison Computation Program");
-        }
+import java.util.Scanner;
+public class LineCompare
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        //Getting values from user
+        System.out.println("Enter the first co-ordinate of x");
+        int x1 = s.nextInt();
+        System.out.println("Enter the first co-ordinate of y");
+        int y1 = s.nextInt();
+        System.out.println("Enter the second co-ordinate of x");
+        int x2 = s.nextInt();
+        System.out.println("Enter the second co-ordinate of y");
+        int y2 = s.nextInt();
+        //equation to get solved
+        double line_length = Math.sqrt((Math.pow((x2 - x1) , 2 )+Math.pow((y2 - y1) , 2 )));
+        //Printing the length of line
+        System.out.println("The length of line is" +line_length);
     }
 }
