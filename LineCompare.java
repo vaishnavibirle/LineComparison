3430cceece26b959176af8644c9a39d4d68eadb7
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-28 23:48:28 +0530

    [vaishnavi] adding LineCompare.java

---
+++ LineCompare.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class LineCompare {
+//Displaying welcome message
+    public static void main(String[] args) {
+        {
+            System.out.println("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
